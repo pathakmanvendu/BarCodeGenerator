08b0908539901a86a82bf0ab363d91dff829e89c
Manvendu Pathak <manvendupathak2000@gmail.com>
2024-03-22 23:07:59 +0530

    Initial Commit

---
+++ BarCodeGenerator/BarCodeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeGeneratorApp.swift
+//  BarCodeGenerator
+//
+//  Created by manvendu pathak  on 22/03/24.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
