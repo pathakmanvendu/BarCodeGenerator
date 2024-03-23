6b5ee65f08e6e822167060a27668bb3a6a3b4a73
Manvendu Pathak <manvendupathak2000@gmail.com>
2024-03-23 10:24:16 +0530

    First Commit: 23  March, 2024

--- BarCodeGenerator/ContentView.swift
+++ BarCodeGenerator/ContentView.swift
@@ -6,19 +6,62 @@
 //
 
 import SwiftUI
+import CoreImage.CIFilterBuiltins
+import Combine
+
 
 struct ContentView: View {
+    @State private var inputText = ""
+    var barcodeGenerator = BarCodeGenerator()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Barcode Generator")
+                .font(.system(size: 60,weight: .bold, design: .rounded))
+            Text("Please enter your data inside the textfield")
+                .font(.headline)
+                .padding(.bottom, 20)
+            
+            TextField("",text: $inputText)
+                .padding()
+                .font(.title)
+                .background(Color(.systemGray6))
+            
+            Spacer()
+            
+            VStack(spacing: 0){
+                barcodeGenerator.generateBarcode(text: inputText)
+                    .resizable()                    .scaledToFit()
+                Text(inputText.isEmpty ? "Unknown Data" : inputText)
+                
+            }
+            
         }
-        .padding()
-    }
+        .padding()    }
 }
 
 #Preview {
     ContentView()
+}
+
+
+struct BarCodeGenerator {
+    let context = CIContext()
+    let generator = CIFilter.code128BarcodeGenerator()
+    
+    func generateBarcode(text: String) -> Image{
+        let generator = CIFilter.code128BarcodeGenerator()
+        generator.message = Data(text.utf8)
+        
+        
+        if let outputImage = generator.outputImage,
+           let cgImage = context.createCGImage(outputImage, from: outputImage.extent){
+            
+            let uiImage = UIImage(cgImage: cgImage)
+            
+            return Image(uiImage: uiImage)
+        }
+        
+        return Image(systemName: "barcode")
+        
+    }
 }
